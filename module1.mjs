6473359fd5cfe044c7089da2bd0e481c66dfec8a
Sapan <sapansidhwani40@gmail.com>
2023-03-04 09:18:36 +0530

    video-6 completed

---
+++ module1.mjs
@@ -0,0 +1,3 @@
+import anyName, {a, c, d} from "./module2.mjs"
+
+console.log(anyName, a, c, d); 
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+var a = "sapan";
+var b = "tapan";
+var c = "chapan";
+var d = "trepan";
+
+export default b;
+export {a};
+export {c};
+export {d};
