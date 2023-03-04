62afd1478dea008d6d2532d163dba6288836d637
Sapan <sapansidhwani40@gmail.com>
2023-03-04 14:23:23 +0530

    video-10 completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
 
-    {/* Navbar is component , title and about is */}
+      {/* Navbar is component , title and about is */}
       <Navbar title="TextUtils" about="About Us" />
 
       <TextForm heading="Enter the text to analyze below" />
+
+      <About/>
 
     </>
   );
